9aee37a98f2b98a43f3c13860a3282a97c9e5289
gaob <zyzlouis@gmail.com>
2017-01-12 22:22:20 +0800

    add edit&delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @group = @post.group
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path ,notice: "Update post success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path ,alert: "Deleted"
+  end
 
   private
 
